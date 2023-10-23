cfb624ffeee817224728c08a86e60f32cfae720a
Daven <dchang1iz@gmail.com>
2023-10-22 22:12:44 -0400

    v1

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def shift_down():
+    global a
+    np.where()
+
+
+a = np.zeros(shape=(8, 4))
+a[6:8] = np.array([[0, 0, 1, 0],
+                   [1, 1, 1, 0]])
+a[0:2] = np.array([[0, 2, 2, 0],
+                   [2, 2, 0, 0]])
+
+
+print(a)
+[2, 2, 2, 0]
